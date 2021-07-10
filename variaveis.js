dc64029808c9fe44c2a30fd3039c59d1085d7c23
Marcos <saoroque@hotmail.com>
2021-07-10 19:24:48 -0300

    Variaveis

---
+++ variaveis.js
@@ -0,0 +1,36 @@
+// var, let, const
+
+// var -> ES5 - cross-browser
+// let e const -> ES6(2015) - Todos Browsers Modernos
+
+let teste = "minha string";
+teste = 10;
+console.log(teste);
+/* let não recria variável e var sim (perigoso)
+let teste = "minha string";
+let teste = 10;
+console.log(teste);
+*/
+
+/* let não recria variável e var sim (perigoso)
+var teste = "minha string";
+var teste = 10; (perigo)
+console.log(teste);
+*/
+
+/* Const não reatribui valor e let sim
+const teste2 = 11;
+teste = 12; // erro
+console.log(teste);
+*/
+
+/* let pode ser declarada sem valor, const não
+let teste3;
+const teste3 = 20;
+*/
+
+/* Navegação VSCode
+Ctrl + Alt + seta pra baixo: Duplica linha para edição
+Alt + seta cima e baixo: muda de linha
+
+*/
