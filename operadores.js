3e816a555f5b314df37e1285aeb35af3eba62185
Marcos <saoroque@hotmail.com>
2021-08-04 23:07:16 -0300

    Operadores

---
+++ operadores.js
@@ -0,0 +1,21 @@
+// + - * / % **
+let n1 = 10
+let n2 = 5
+// OPERADORES ARITMÉTICOS
+console.log(n1 + n2)
+console.log(n1 - n2)
+console.log(n1 * n2)
+console.log(n1 / n2)
+console.log(n1 % n2)
+console.log(n1 ** n2)
+
+// OPERADORES DE ATRIBUIÇÃO
+let n3 = 20;
+// n3 = n3 + 15;
+// n3 += 15;
+// n3 -= 15;
+// n3 *= 15;
+// n3 /= 15;
+// n3 %= 15;
+n3 **= 15;
+console.log(n3);
