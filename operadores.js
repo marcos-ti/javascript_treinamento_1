4e4c66f45dc05a6eb3f87cf2a84e02c3ca2db28f
Marcos <saoroque@hotmail.com>
2021-08-30 19:44:11 -0300

    Operadores de Incremento e Decremento

--- operadores.js
+++ operadores.js
@@ -1,21 +1,38 @@
 // + - * / % **
-let n1 = 10
-let n2 = 5
+//let n1 = 10
+//let n2 = 5
 // OPERADORES ARITMÉTICOS
-console.log(n1 + n2)
-console.log(n1 - n2)
-console.log(n1 * n2)
-console.log(n1 / n2)
-console.log(n1 % n2)
-console.log(n1 ** n2)
+// console.log(n1 + n2)
+// console.log(n1 - n2)
+// console.log(n1 * n2)
+// console.log(n1 / n2)
+// console.log(n1 % n2)
+// console.log(n1 ** n2)
 
 // OPERADORES DE ATRIBUIÇÃO
-let n3 = 20;
+// let n3 = 20;
 // n3 = n3 + 15;
 // n3 += 15;
 // n3 -= 15;
 // n3 *= 15;
 // n3 /= 15;
 // n3 %= 15;
-n3 **= 15;
-console.log(n3);
+//n3 **= 15;
+//console.log(n3);
+
+// INCREMENTO E DECREMENTO
+let i = 0;
+//i = i + 1;
+//i += 1;
+// i++;
+//i--;
+// ++i;
+// --i;
+
+// Primeiro atribui o valor 0 à variavel e depois soma 1
+// console.log(i++);
+// console.log(i);
+
+// Primeiro soma 1 à variavel e depois mostra a soma
+console.log(++i);
+console.log(i);
