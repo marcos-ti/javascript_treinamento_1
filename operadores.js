8bc4296c4057aa09110ec19431b372718c239ecc
Marcos <saoroque@hotmail.com>
2021-08-31 10:41:04 -0300

    Operadores Logicos

--- operadores.js
+++ operadores.js
@@ -45,11 +45,32 @@
  * != valores diferentes
  * !== valores e tipos diferentes
  */
+// let n1 = 10;
+// let n2 = 5;
+// let n3 = 20;
+// let n4 = '10';
+
+// // console.log(n1 == n4);
+// console.log(n1 === n4); // false
+// console.log(n1 == n4); // true
+
 let n1 = 10;
 let n2 = 5;
 let n3 = 20;
 let n4 = '10';
 
-// console.log(n1 == n4);
-console.log(n1 === n4); // false
-console.log(n1 == n4); // true
+// OPERADORES LOGICOS
+ /**
+  * AND (&&)
+  * OR (||)
+  * NOT (!)
+  * 
+  * Para uma pessoa viajar para o exterior:
+  * - Precisa ser maior de 18
+  * OU
+  * - Acompanhado(a) dos pais
+  */
+ let idade = 21;
+ let paisPresentes = false;
+ const podeViajar = idade >= 18 || paisPresentes;
+ console.log(`Pode viajar: ${podeViajar}`);
