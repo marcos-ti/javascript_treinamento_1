92fd7f26352748db8979923c6b39ea5539afeeae
Marcos <saoroque@hotmail.com>
2021-08-31 23:10:40 -0300

    Falsy-Truthy

---
+++ falsy-truthy.js
@@ -0,0 +1,10 @@
+// Falsy values: 0, "", NaN, undefined, null, false
+// Truth: Todos os demais
+
+if(0){
+    console.log("Este valor é verdadeiro");
+    } else {
+        console.log("Este valor é falso");
+    }
+
+
