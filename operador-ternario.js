bb8db8190e8bbdf9ea8bd8ba713a6e2786db511f
Marcos <saoroque@hotmail.com>
2021-08-31 21:55:54 -0300

    Operador Ternario

---
+++ operador-ternario.js
@@ -0,0 +1,12 @@
+// Operador Ternario
+let idade = 18;
+let comprouBilhete =  true;
+// Parametros ? "true" : "false";
+let msgMaiorIdade = (idade >= 18) ? "É maior de idade" : "É menor de 18";
+
+if(!comprouBilhete){
+    console.log("Não comprou o bilhete")
+    } else {
+        console.log(msgMaiorIdade);
+    };
+
