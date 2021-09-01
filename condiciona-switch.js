a97b61da1e1b825ac9ac14dd5034276e488bbab0
Marcos <saoroque@hotmail.com>
2021-09-01 17:41:20 -0300

    Switch

---
+++ condiciona-switch.js
@@ -0,0 +1,48 @@
+// Com If Else
+// let diaSemana = 5;
+
+// if(diaSemana === 0){
+//     console.log("Hoje é Domingo")
+// } else if(diaSemana === 1){
+//     console.log("Hoje é Segunda")
+// } else if(diaSemana === 2){
+//     console.log("Hoje é Terça")
+// } else if(diaSemana === 3){
+//     console.log("Hoje é Quarta")
+// } else if(diaSemana === 4){
+//     console.log("Hoje é Quinta")
+// } else if(diaSemana === 5){
+//     console.log("Hoje é Sexta")
+// } else {
+//     console.log("Hoje é Sábado")
+// };
+
+// Com Swicth
+let diaSemana = 9;
+let dia = "";
+switch(diaSemana){
+    case 0:
+        dia = "Domingo"
+        break
+    case 1:
+        dia = "Segunda"
+        break
+    case 2:
+        dia = "Terça"
+        break
+    case 3:
+        dia = "Quarta"
+        break
+    case 4:
+        dia = "Quinta"
+        break
+    case 5:
+        dia = "Sexta"
+        break
+    case 6:
+        dia = "Sábado"
+        break
+    default:
+        dia = ' -- '
+}
+console.log(`Hoje o dia é: ${dia}`);
