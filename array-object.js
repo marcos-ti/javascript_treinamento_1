c2d34ad7a7c07f51fb4a7ffc216a9cb78d24ba41
Marcos <saoroque@hotmail.com>
2021-09-07 18:09:07 -0300

    Array de Objetos

---
+++ array-object.js
@@ -0,0 +1,17 @@
+const pessoas = [{
+    nome: "Marcos",
+    idade: 43
+},{
+    nome: "Maria",
+    idade: 28
+},{
+    nome: "Helena",
+    idade: 34
+},{
+    nome: "João",
+    idade: 23
+}]
+
+console.log(pessoas)
+console.log(pessoas[0])
+console.log(pessoas[0].nome)
