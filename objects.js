d692ddb765a9681d11754268fe9823f655bf08f2
Marcos <saoroque@hotmail.com>
2021-09-07 10:11:20 -0300

    Objetos

---
+++ objects.js
@@ -0,0 +1,26 @@
+/**Com variaveis */
+//const nomes = ["Marcos", "Maria", "Joao"]
+//cont idades = [null, 40 , 50, 60]
+//console.log(nome[0], idade[0])
+
+// Objeto com linguagem formal
+// const pessoa = new Object()
+// pessoa.nome = "Marcos"
+// pessoa.idade = 43
+
+// let prop = "nome"
+// console.log(pessoa["nome"])
+// console.log(pessoa[prop])
+// console.log(pessoa.nome)
+// console.log(pessoa.idade)
+
+// Objeto na linguagem literal
+const pessoa = {
+    nome: "Marcos",
+    idade: 43
+}
+let prop = "nome"
+console.log(pessoa["nome"])
+console.log(pessoa[prop])
+console.log(pessoa.nome)
+console.log(pessoa.idade)
